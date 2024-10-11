e04367b5ca8a8ed107c7becee273b6b44cebd283
Kehinde <ojodamilarey@gmail.com>
2024-10-11 15:50:49 +0100

    Update App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import SearchResults from './pages/SearchResults';
+import VideoPlayer from './pages/VideoPlayer';
+import Sidebar from './components/Sidebar';
+import VideoCard from './components/VideoCard';
+import VideoDetail from './components/VideoDetail';
+import VideoList from './components/VideoList';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search/:searchQuery" element={<SearchResults />} />
+        <Route path="/video/:videoId" element={<VideoPlayer />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
